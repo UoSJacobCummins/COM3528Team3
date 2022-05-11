2a2876f22f457eb225e75f4d563da7ef45528002
Robotics Student <student@TENB88584BB25A7.shefuniad.shef.ac.uk>
2022-05-11 17:08:02 +0100

    cost function added

--- src/cost.py
+++ src/cost.py
@@ -4,4 +4,6 @@ def cost(rotations):
     return (30 * math.log(rotations + 2))
 
 def cost_constant_stimulus(rotations, utility):
+    if(rotations < 0):
+        rotations = 0
     return utility / cost(rotations)
